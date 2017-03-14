8eaafa12b88ceb088be8abcc6667ea251253e88d
elad wertzberger <eladw@liveprson.com>
2017-03-14 22:29:42 +0200

    add sample path param

--- facade/src/main/java/com/elad/RestContorller.java
+++ facade/src/main/java/com/elad/RestContorller.java
@@ -2,10 +2,7 @@ package com.elad;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by eladw on 3/14/17.
@@ -23,6 +20,17 @@ public class RestContorller {
 
         System.out.print("Elad test get!!");
         return "Elad test get!!";
+    }
+
+    @RequestMapping(value = "management/userId/{userId}/userName/{userName}/accountId/{accountId}", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    @ResponseBody
+    public String postAccessToken(@PathVariable("userId") int userId, @PathVariable("userName") String userName,
+                                  @PathVariable("accountId") String accountId) throws Exception {
+        System.out.print("**** Parameters POST: userId:" + userId + " ,userName:" + userName
+                + " accountid:" + accountId);
+
+        return "elad";
     }
 
 }
